c3c4305f17342a031c0f003eed9bb61b8bf3dff4
JosephASandoval <josephasandoval17@gmail.com>
2021-05-24 00:20:42 -0700

    wording is fixed

--- src/components/complete.jsx
+++ src/components/complete.jsx
@@ -4,16 +4,14 @@ import { getMessage } from "../data/messages";
 const Complete = ({ numTimes, score, quizLength, onRetake }) => {
   return (
     <>
-      <h1>
-        You have completed this quiz the following number of times:{" "}
-        <span>{numTimes}</span>
-      </h1>
       <p>
-        You correctly answered <span>{score}</span> out of{" "}
-        <span>{quizLength}</span> questions, which is a score of{" "}
-        <span>{Math.floor((score / quizLength) * 100)}%</span>
+        You got <span>{score}</span> out of <span>{quizLength}</span> questions
+        right.
       </p>
       <h2>{getMessage()}</h2>
+      <p>
+        This was attempt number <span>{numTimes}</span>
+      </p>
       <button onClick={onRetake} className="next">
         Retake
       </button>
