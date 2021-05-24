f73eee3c48334e73185fc903a87d1e68d4207a71
JosephASandoval <josephasandoval17@gmail.com>
2021-05-24 11:45:05 -0700

    updated unit tests

--- src/app.test.js
+++ src/app.test.js
@@ -20,7 +20,8 @@ test("when the user chooses the correct answer", () => {
 test("when the user chooses the incorrect answer", () => {
   let component = renderer.create(<Quiz />).getInstance();
   let currentQuiz = 0;
-  let currentQuestion = 0;
+  let currentQuizLength = quizzes[currentQuiz].questions.length;
+  let currentQuestion = Math.floor(Math.random() * currentQuizLength);
   let incorrectAnswersLength =
     quizzes[currentQuiz].questions[currentQuestion].incorrectAnswers.length;
   let answerOption =
